4136d90ad0f413cdac92d49ee3047828abd9da58
Vladislav Mickevic <vladislav.mickevic@sticky.ad>
2013-08-20 15:50:20 +0200

    making schedule configurable

--- data_backup/recipes/default.rb
+++ data_backup/recipes/default.rb
@@ -1,8 +1,8 @@
 cron "puzzle data backup" do
-  hour "15"
-  minute "0"
-  weekday "2"
+  hour "$BACKUP_SCHEDULE_HOUR"
+  minute "$BACKUP_SCHEDULE_MINUTE"
+  weekday "$BACKUP_SCHEDULE_WEEKDAY"
   command "rm -rf dump && " + 
-  			"for COL in $COLLECTIONS; do mongoctl dump $SRC_DB -c $COL; done && " +
-  			"mongoctl restore $DST_DB dump/beta --drop"
+  			"for COL in $BACKUP_COLLECTIONS; do mongoctl dump $BACKUP_SRC_DB -c $COL; done && " +
+  			"mongoctl restore $BACKUP_DST_DB dump/beta --drop"
 end

c71e77e743676b8f06b6ea83153127f46e5cbfbf
Vladislav Mickevic <vladislav.mickevic@sticky.ad>
2013-08-20 16:26:48 +0200

    hardcoding schedules

--- data_backup/recipes/default.rb
+++ data_backup/recipes/default.rb
@@ -1,7 +1,7 @@
 cron "puzzle data backup" do
-  hour "$BACKUP_SCHEDULE_HOUR"
-  minute "$BACKUP_SCHEDULE_MINUTE"
-  weekday "$BACKUP_SCHEDULE_WEEKDAY"
+  hour "16"
+  minute "30"
+  weekday "2"
   command "rm -rf dump && " + 
   			"for COL in $BACKUP_COLLECTIONS; do mongoctl dump $BACKUP_SRC_DB -c $COL; done && " +
   			"mongoctl restore $BACKUP_DST_DB dump/beta --drop"

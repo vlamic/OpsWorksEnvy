7e6635f70438a38f8cc1a540c10b42a62e736b4a
Vladislav Mickevic <vladislav.mickevic@sticky.ad>
2013-08-21 14:05:59 +0200

    Providing sample configuration file for mongoctl

--- data_backup/recipes/install.rb
+++ data_backup/recipes/install.rb
@@ -1,3 +1,15 @@
+Chef::Log.info("Creating sample configuration mongodb file")
+
+FileUtils.mkdir("/root/.mongoctl")
+
+template "/root/.mongoctl/mongoctl.config" do
+	source "mongoctl.config.erb"
+	mode "0440"
+	owner "root"
+	group "root"
+end
+
+Chef::Log.info("Installing mongoctl and mongodb")
 script "install pip and mongoctl" do
 	interpreter "bash"
 	user "root"
